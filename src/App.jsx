bec4109d29cebd9294e607a113be9f51c78fa8c3
vhrobinson <vhrobinson@gmail.com>
2024-12-06 23:06:39 -0300

    feat(App.jsx):se comenta componente Home, se crear componente Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Home from './pages/Home'
-import {pizza} from './data/pizzas'
+import Cart from './pages/components/Cart'
+//import Home from './pages/Home'
+
 function App() {
   return (
     <>
       <div className='flex flex-col min-h-screen'>
         <Navbar />
-        <div className='flex-grow'>
+        {/* <div className='flex-grow'>
           <Home />
-        </div>
+        </div>  según lo indicado en el desafio */}
+        <Cart/>
         <Footer />
       </div>
     </>
